36aec192098026da418b1ab44ca4c357e4ccc9ca
Gauravjeetsingh <gaurav.ishwerdas@gmail.com>
2014-10-12 01:00:21 +0530

    Added contribute part and fixed typos

--- js/scroll-effect.js
+++ js/scroll-effect.js
@@ -8,7 +8,7 @@
 
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
-    var maxheight = $("#home").height() + 230;
+    var maxheight = $("#home").height() + 70;
     if (scroll >= maxheight) {
         $(".header").addClass( "small-header");
     } else {
@@ -18,17 +18,21 @@ $(window).scroll(function() {
 
 
 $(".home-menu").click(function() {
-    $('html, body').animate({ scrollTop:$("#home").offset().top - 100}, 500);
+    $('html, body').animate({ scrollTop:$("#home").offset().top}, 500);
 });
 
 $(".team-menu").click(function() {
-    $('html, body').animate({ scrollTop:$("#team").offset().top - 50}, 500);
+    $('html, body').animate({ scrollTop:$("#team").offset().top + 100}, 500);
 });
 
 $(".install-menu").click(function() {
-    $('html, body').animate({ scrollTop:$("#install").offset().top - 20}, 500);
+    $('html, body').animate({ scrollTop:$("#install").offset().top + 130}, 500);
 });
 
 $(".contact-menu").click(function() {
-    $('html, body').animate({ scrollTop:$("#contact").offset().top - 20}, 500);
+    $('html, body').animate({ scrollTop:$("#contact").offset().top - 30}, 500);
+});
+
+$(".contribute-menu").click(function() {
+    $('html, body').animate({ scrollTop:$("#contribute").offset().top + 100 }, 500);
 });
